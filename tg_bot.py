86b21cd59dc497aaf564d98f3b6595879a26b8d5
iterekhov98@gmail.com <iterekhov98@gmail.com>
2022-06-25 17:16:27 +0300

    Set states

--- tg_bot.py
+++ tg_bot.py
@@ -1,10 +1,13 @@
+import asyncio
 import os
 import shutil
 import logging
 from typing import List
 
 from environs import Env
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher.filters import MediaGroupFilter
 from aiogram_media_group import media_group_handler
 from aiogram.types import ContentType
@@ -19,6 +22,10 @@ EXAMPLES_DIR = 'examples/'
 img_order = {'content': 0, 'style': 1, 'result': 2}
 
 
+class BotStates(StatesGroup):
+    waiting_for_photos = State()
+
+
 async def start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button_1 = types.KeyboardButton(text="Хочу перенести стиль на фото")
@@ -31,7 +38,7 @@ async def show_examples(message):
     for root, dirs, images in os.walk(EXAMPLES_DIR):
         await types.ChatActions.upload_photo()
         media = types.MediaGroup()
-        for image in sorted(images, key= lambda x: img_order[x.split('.')[0]]):
+        for image in sorted(images, key=lambda x: img_order[x.split('.')[0]]):
             path_img = f'{root}/{image}'
             media.attach_photo(types.InputFile(path_img), image[:-4])
         if images:
@@ -44,10 +51,11 @@ async def handle_style_transfer(message):
         1 послужит примером стиля, а на второй я постараюсь\
         его перенести. Не перепутай )'''
     )
+    await BotStates.waiting_for_photos.set()
 
 
 @media_group_handler
-async def download_photo(messages: List[types.Message]):
+async def download_photo(messages: List[types.Message], state: FSMContext):
     images = []
     for message in messages:
         buffer = await message.photo[-1].download(
@@ -61,9 +69,10 @@ async def download_photo(messages: List[types.Message]):
     media.attach_photo(types.InputFile(styled_photo_path))
     await message.reply_media_group(media=media)
     shutil.rmtree(photos_dir)
+    await state.finish()
 
 
-def main():
+async def main():
     logger.setLevel(logging.INFO)
     env = Env()
     env.read_env()
@@ -73,19 +82,23 @@ def main():
     dp.register_message_handler(start, commands="start")
     dp.register_message_handler(
         show_examples,
-        lambda message: message.text == 'Покажи примеры своих скиллов'
+        lambda message: message.text == 'Покажи примеры своих скиллов',
+        state="*"
     )
     dp.register_message_handler(
         handle_style_transfer,
-        lambda message: message.text == "Хочу перенести стиль на фото"
+        lambda message: message.text == "Хочу перенести стиль на фото",
+        state="*"
     )
     dp.register_message_handler(
         download_photo,
         MediaGroupFilter(is_media_group=True),
-        content_types=ContentType.PHOTO
+        content_types=ContentType.PHOTO,
+        state=BotStates.waiting_for_photos
     )
-    executor.start_polling(dp, skip_updates=True)
+    await dp.skip_updates()
+    await dp.start_polling()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
